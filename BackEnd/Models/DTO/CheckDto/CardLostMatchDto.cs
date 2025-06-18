a831aac05ffa7ac0c8d8b3e591ed826559420c0c
Ahmed Sayed <ahmed9pato7@gmail.com>
2025-06-18 21:09:17 +0300

    DONE

---
+++ BackEnd/Models/DTO/CheckDto/CardLostMatchDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Lost_and_Found.Models.DTO.CheckDto
+{
+    public class CardLostMatchDto
+    {
+        public string name { get; set; }
+        public string national_id { get; set; }
+        public string governorate { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+        public string contact { get; set; }
+        public string image_name { get; set; }
+    }
+}
